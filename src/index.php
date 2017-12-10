2074e77ba7115da8846878acd45d4002d565e3a8
LYNDWYN\Laura <laura.steiner@nyp.ch>
2017-12-10 11:31:10 +0100

    Login Mask createt, ready for test

---
+++ src/index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <!--<link rel="stylesheet" type="text/css" href="screen.css" />-->
+    </head>
+    <body>
+    <div id="inhalt_links">
+        <a href="system/loginMask.php" >Login</a >
+    </div>
+    </body>
+</html>
